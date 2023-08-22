d5d282b8d4c737f4c4fcb2e3f45514d02a5ba136
sefx5ever <06170171@mss.scu.edu.tw>
2023-08-22 06:26:23 +0000

    v1.0: Create n App 1 step's code

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ AwsCdkModernCloudArchitectEvolutionStack(app, "AwsCdkModernCloudArchitectEvoluti
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='274251673384', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- aws_cdk_modern_cloud_architect_evolution/aws_cdk_modern_cloud_architect_evolution_stack.py
+++ aws_cdk_modern_cloud_architect_evolution/aws_cdk_modern_cloud_architect_evolution_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2
 )
 from constructs import Construct
 
@@ -10,10 +9,20 @@ class AwsCdkModernCloudArchitectEvolutionStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # App 1：EC2 with all default
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkModernCloudArchitectEvolutionQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        ## Select the default vpc
+        vpc = ec2.Vpc.from_lookup(self, "VPC",
+            vpc_id="vpc-04bc3634aea0e5fc5"                          
+        )
+
+        ## Create an EC2 of Amazon Linux 2 Instance 
+        ec2.Instance(self, "Instance",
+            vpc=vpc,
+            instance_type=ec2.InstanceType.of(
+                instance_class=ec2.InstanceClass.T3,
+                instance_size=ec2.InstanceSize.MICRO
+            ),
+            machine_image=ec2.MachineImage.latest_amazon_linux2()
+        )
+
